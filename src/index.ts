f11e04aa0e4ed893047e43c42a0fe8b0e1e2d026
Grant Timmerman <granttimmerman@gmail.com>
2019-06-26 21:36:54 -0700

    feat: Add gitencoder

--- src/index.ts
+++ src/index.ts
@@ -10,52 +10,54 @@ import { loadCredentials} from './auth';
 const download = require('download-file');
 const readline = require('readline');
 
-const oauth2ClientSettings = {
-  clientId: '833834422750-hr0a48ds7l0ofvvvdhnk6k4n3o2a51kp.apps.googleusercontent.com',
-  clientSecret: 'gA_p1p_0y_qywHX0IYnz0X2b',
-  redirectUri: 'http://localhost',
-};
-let globalOauth2Client = new OAuth2Client(oauth2ClientSettings);
-const slides = google.slides({ version: 'v1', auth: globalOauth2Client }) as slides_v1.Slides;
+// const oauth2ClientSettings = {
+//   clientId: '833834422750-hr0a48ds7l0ofvvvdhnk6k4n3o2a51kp.apps.googleusercontent.com',
+//   clientSecret: 'gA_p1p_0y_qywHX0IYnz0X2b',
+//   redirectUri: 'http://localhost',
+// };
+// let globalOauth2Client = new OAuth2Client(oauth2ClientSettings);
+// const slides = google.slides({ version: 'v1', auth: globalOauth2Client }) as slides_v1.Slides;
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/presentations.readonly'];
+// // If modifying these scopes, delete token.json.
+// const SCOPES = ['https://www.googleapis.com/auth/presentations.readonly'];
 
-loadCredentials(async () => {
-  let slidesId = '1TOZawYQsYFzqd_gf1ZZyuhlXycio3Ylh-HAF_qz_5qU';
-  let p = await slides.presentations.get({
-    presentationId: slidesId,
-  })
-  if (p.data.slides) {
-    console.log(`${p.data.slides.length} slides.`);
-  }
-  let presoSlides = p.data.slides;
-  if (!presoSlides) return;
+console.log('hi');
 
-  var i = 0;
-  for (let slide of presoSlides) {
-    const thumbnail = await slides.presentations.pages.getThumbnail({
-      presentationId: slidesId,
-      pageObjectId: slide.objectId,
-    });
-    let {
-      contentUrl, // png
-      height,
-      width,
-    } = thumbnail.data;
-    console.log(`${contentUrl} (${width}x${height})`);
+// loadCredentials(async () => {
+//   let slidesId = '1TOZawYQsYFzqd_gf1ZZyuhlXycio3Ylh-HAF_qz_5qU';
+//   let p = await slides.presentations.get({
+//     presentationId: slidesId,
+//   })
+//   if (p.data.slides) {
+//     console.log(`${p.data.slides.length} slides.`);
+//   }
+//   let presoSlides = p.data.slides;
+//   if (!presoSlides) return;
 
-    var options = {
-      directory: "./images/",
-      filename: `${i}.png`
-    }
+//   var i = 0;
+//   for (let slide of presoSlides) {
+//     const thumbnail = await slides.presentations.pages.getThumbnail({
+//       presentationId: slidesId,
+//       pageObjectId: slide.objectId,
+//     });
+//     let {
+//       contentUrl, // png
+//       height,
+//       width,
+//     } = thumbnail.data;
+//     console.log(`${contentUrl} (${width}x${height})`);
 
-    // Test image:
-    // var contentUrl = "http://i.imgur.com/G9bDaPH.jpg"
-    download(contentUrl, options, (err: any) => {
-      if (err) throw err
-    });
-    ++i;
-  }
-  console.log('good5');
-});
+//     var options = {
+//       directory: "./images/",
+//       filename: `${i}.png`
+//     }
+
+//     // Test image:
+//     // var contentUrl = "http://i.imgur.com/G9bDaPH.jpg"
+//     download(contentUrl, options, (err: any) => {
+//       if (err) throw err
+//     });
+//     ++i;
+//   }
+//   console.log('good5');
+// });
